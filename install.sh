20a7c57fe595f4886ad92f46820564f7c78ae3dc
Matthew Padden <matthewpaddendev@gmail.com>
2024-10-25 16:14:43 +0100

    feat: add tmux plugins
    Add plugins to repo along with command to set up symbolic link for .tmux
    directory.

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ sudo apt install neovim
 
 # Create symbolic links
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.tmux ~/.tmux
 ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
